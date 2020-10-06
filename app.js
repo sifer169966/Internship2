43c67c74e0af3e67ff5c4fd2544bf1e948256fb5
sifer169966 <doncan@Wasins-MacBook-Pro.local>
2020-10-06 18:43:34 +0700

    conflict

--- app.js
+++ app.js
@@ -10,19 +10,11 @@ require("dotenv/config");
 /*===================================================================================== */
 /*                                  MIDDLEWARES                                         */
 /*===================================================================================== */
-<<<<<<< HEAD
-app.use(cors())
-app.use(logger('dev'))
-app.use(methodOverride('_method'));
-app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(bodyparser.json())
-=======
 app.use(cors()); //fetch cross domain
 app.use(logger("dev"));
 app.use(methodOverride("_method"));
 app.use(methodOverride("X-HTTP-Method-Override"));
 app.use(bodyparser.json());
->>>>>>> 1e7fce488077a750d6767fcc8ed99df4c1f2097d
 app.use(bodyparser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
@@ -48,8 +40,4 @@ app.use("/", firstpage);
 const StoreRoute = require("./routes/store");
 app.use("/store", StoreRoute);
 
-<<<<<<< HEAD
-app.listen(3000)
-=======
 app.listen(4000);
->>>>>>> 1e7fce488077a750d6767fcc8ed99df4c1f2097d
