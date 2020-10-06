dac395a1e74fbf85390ba36a43d5ef58ea9934c2
Chanapai Suparbpong <chanapai.s@existing.co>
2020-10-06 18:33:29 +0700

    REMOVE comment CORS and change port

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
-const express = require('express')
-const bodyparser = require('body-parser')
-const mongoose = require('mongoose')
-const cors = require('cors')  //cors use for cross domain fetching
-const logger = require('morgan')
-const methodOverride = require('method-override');
-let app = express()
-require('dotenv/config')
-
+const express = require("express");
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors"); //cors use for cross domain fetching
+const logger = require("morgan");
+const methodOverride = require("method-override");
+let app = express();
+require("dotenv/config");
 
 /*===================================================================================== */
 /*                                  MIDDLEWARES                                         */
 /*===================================================================================== */
-app.use(cors())//fetch cross domain
-app.use(logger('dev'))
-app.use(methodOverride('_method'));
-app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(bodyparser.json())
+app.use(cors());
+app.use(logger("dev"));
+app.use(methodOverride("_method"));
+app.use(methodOverride("X-HTTP-Method-Override"));
+app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 /*===================================================================================== */
 /*                                  CONNECT TO DATABASE                                 */
 /*===================================================================================== */
 
-mongoose.connect(process.env.DB_CONNECTIONTUTORIAL, {useNewUrlParser: true}, ()=>{
-    console.log('Connect to DB Tutorial')
-})
+mongoose.connect(
+  process.env.DB_CONNECTIONTUTORIAL,
+  { useNewUrlParser: true },
+  () => {
+    console.log("Connect to DB Tutorial");
+  }
+);
 
 /*===================================================================================== */
 /*                                  CONTROLLER ROUTES                                   */
 /*===================================================================================== */
 
-const firstpage = require('./routes/firstpage')
-app.use('/', firstpage)
+const firstpage = require("./routes/firstpage");
+app.use("/", firstpage);
 
-const StoreRoute = require('./routes/store')
-app.use('/store', StoreRoute)
+const StoreRoute = require("./routes/store");
+app.use("/store", StoreRoute);
 
-
-app.listen(3000)
+app.listen(4000);
